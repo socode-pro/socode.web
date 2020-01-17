4c7aa0b62654dd3842053766f8777107c746f5ca
zicjin <zicjin@gmail.com>
2020-01-17 13:22:51 +0800

    temp

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,22 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// https://stackoverflow.com/a/58551905/346701
+document.addEventListener('DOMContentLoaded', e => {
+  if (!window.matchMedia)
+    return
+
+  const current = document.querySelectorAll('head > link[rel*="icon"][media]')
+  current.forEach((icon: any) => {
+    const match = window.matchMedia(icon.media)
+    function swap() {
+      if (match.matches) {
+        current.forEach(c => c.parentNode?.removeChild(c))
+        document.head.appendChild(icon)
+      }
+    }
+    match.addListener(swap)
+    swap()
+  })
+})
