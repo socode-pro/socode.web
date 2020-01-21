c836ef21a1a0567885cb27fd364b34d503a02114
zicjin <zicjin@gmail.com>
2020-01-21 15:34:31 +0800

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 // https://stackoverflow.com/a/58551905/346701
 document.addEventListener('DOMContentLoaded', e => {
-  if (!window.matchMedia)
-    return
+  if (!window.matchMedia) return
 
   const current = document.querySelectorAll('head > link[rel*="icon"][media]')
   current.forEach((icon: any) => {
