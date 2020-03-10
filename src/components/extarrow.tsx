ee8001eb94d43c9e1507bfd6971b6454bd1a2bb0
zicjin <zicjin@gmail.com>
2020-03-10 22:31:30 +0800

    temp

--- src/components/extarrow.tsx
+++ src/components/extarrow.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import cs from 'classnames'
 import css from './extarrow.module.scss'
 
+const isChrome = !!(window as any).chrome && (!!(window as any).chrome.webstore || !!(window as any).chrome.runtime)
+const isEdgeChromium = isChrome && (navigator.userAgent.indexOf('Edg') !== -1)
+
 const ExtArrow: React.FC = (): JSX.Element => {
   return (
     <div className={css.extwapper}>
       <a
-        className={cs(css.arrow)}
-        href='https://chrome.google.com/webstore/detail/socode/hlkgijncpebndijijbcakkcefmpniacd'>
+        className={cs(css.arrow, { [css.edge]: isEdgeChromium })}
+        href='https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/'>
         <h3>Browser Extension</h3>
         <span>become the new tab of your browser</span>
       </a>
