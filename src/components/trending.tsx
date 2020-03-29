50fc712b7b4b16488cea316cc960fc8e5640036c
zicjin <zicjin@gmail.com>
2020-03-29 17:12:20 +0800

    temp

--- src/components/trending.tsx
+++ src/components/trending.tsx
@@ -23,58 +23,60 @@ const Trending: React.FC = (): JSX.Element => {
 
   return (
     <div className={cs(css.trending)}>
-      <div className={css.header}>
-        <div className='select is-small'>
-          <select value={spoken} onChange={e => setSpoken(e.target.value as TrendingSpokenLanguage)}>
-            {spokenLanguageOptions.map(o => (
-              <option key={o.value} value={o.value}>
-                {o.label}
-              </option>
-            ))}
-          </select>
+      <div className={cs(css.container)}>
+        <div className={css.header}>
+          <div className='select is-small'>
+            <select value={spoken} onChange={e => setSpoken(e.target.value as TrendingSpokenLanguage)}>
+              {spokenLanguageOptions.map(o => (
+                <option key={o.value} value={o.value}>
+                  {o.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='select is-small'>
+            <select value={programLanguage} onChange={e => setProgramLanguage(parseInt(e.target.value, 10))}>
+              {programLanguageOptions.map(o => (
+                <option key={o.value} value={o.value}>
+                  {o.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='select is-small'>
+            <select value={since} onChange={e => setSince(e.target.value as TrendingSince)}>
+              {trendingSinceOptions.map(o => (
+                <option key={o.value} value={o.value}>
+                  {o.label}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className='select is-small'>
-          <select value={programLanguage} onChange={e => setProgramLanguage(parseInt(e.target.value, 10))}>
-            {programLanguageOptions.map(o => (
-              <option key={o.value} value={o.value}>
-                {o.label}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className='select is-small'>
-          <select value={since} onChange={e => setSince(e.target.value as TrendingSince)}>
-            {trendingSinceOptions.map(o => (
-              <option key={o.value} value={o.value}>
-                {o.label}
-              </option>
-            ))}
-          </select>
-        </div>
+
+        {loading && <Loader type={2} gray />}
+
+        {!loading && <ul className={css.result}>
+          {repositories.map(r => (
+            <li key={r.author + r.name}>
+              <h4>
+                <a href={r.url} target='_blank' rel='noopener noreferrer'>
+                  {r.author}/{r.name}
+                </a>
+                <span className={css.stars}>
+                  <span className={css.allstars}>★{r.stars}</span>
+                  +{r.currentPeriodStars}
+                </span>
+              </h4>
+              <p className={css.description}>
+                {r.language && <span className={css.language}>{r.language}</span>}
+                {r.description}</p>
+            </li>
+          ))}
+        </ul>}
+
+        {!loading && <a className={css.more} href={url} target='_blank' rel='noopener noreferrer'>READ MORE...</a>}
       </div>
-
-      {loading && <Loader type={2} gray />}
-
-      {!loading && <ul className={css.result}>
-        {repositories.map(r => (
-          <li key={r.author + r.name}>
-            <h4>
-              <a href={r.url} target='_blank' rel='noopener noreferrer'>
-                {r.author}/{r.name}
-              </a>
-              <span className={css.stars}>
-                <span className={css.allstars}>★{r.stars}</span>
-                +{r.currentPeriodStars}
-              </span>
-            </h4>
-            <p className={css.description}>
-              {r.language && <span className={css.language}>{r.language}</span>}
-              {r.description}</p>
-          </li>
-        ))}
-      </ul>}
-
-      {!loading && <a className={css.more} href={url} target='_blank' rel='noopener noreferrer'>READ MORE...</a>}
     </div>
   )
 }
