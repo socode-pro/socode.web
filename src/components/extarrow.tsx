e4d169540d3cebdcdd6ce47f9f42996a818b5a36
zicjin <zicjin@gmail.com>
2020-03-30 10:59:08 +0800

    temp

--- src/components/extarrow.tsx
+++ src/components/extarrow.tsx
@@ -8,8 +8,8 @@ const ExtArrow: React.FC = (): JSX.Element => {
     <div className={css.extwapper}>
       <a
         className={cs(css.arrow, { [css.edge]: isEdgeChromium, [css.firefox]: isFirefox })}
-        href={isFirefox? '': 'https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/'}>
-        <h3>Browser Extension{isFirefox? '(coming soon)': ''}</h3>
+        href={isFirefox? 'https://addons.mozilla.org/zh-CN/firefox/addon/socode-search/': 'https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/'}>
+        <h3>Browser Extension</h3>
         <span>become the new tab of your browser</span>
       </a>
     </div>
