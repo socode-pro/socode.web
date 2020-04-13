12e5c9c57076da24201cdbe255c60cbfc8e1d285
zicjin <zicjin@gmail.com>
2020-04-13 15:25:58 +0800

    temp

--- src/models/trending.ts
+++ src/models/trending.ts
@@ -80,15 +80,16 @@ const trendingModel: TrendingModel = {
     const { programLanguage } = getStoreState().storage
     const language =  ProgramLanguage[programLanguage].toLowerCase().replace(' ', '-').replace('#', '%23')
 
-    const params = localStorage.getItem('repos_params')
-    const times = localStorage.getItem('repos_times')
-    if (
-      params && params === spoken + language + since &&
-      times && dayjs(times).add(2, 'hour').isAfter(dayjs())
-    ) {
-      const repos = JSON.parse(localStorage.getItem('repos') || '[]')
-      actions.setRepositorys(repos)
-    } else {
+    // storageService.getLocal 与 ky.get 执行时间不确定，可能出现逻辑错误。
+    // const params = localStorage.getItem('repos_params')
+    // const times = localStorage.getItem('repos_times')
+    // if (
+    //   params && params === spoken + language + since &&
+    //   times && dayjs(times).add(2, 'hour').isAfter(dayjs())
+    // ) {
+    //   const repos = JSON.parse(localStorage.getItem('repos') || '[]')
+    //   actions.setRepositorys(repos)
+    // } else {
       actions.setLoading(true)
       try {
         const searchParams = new URLSearchParams()
@@ -98,15 +99,15 @@ const trendingModel: TrendingModel = {
           searchParams.set('language', language)
         }
         const data = await ky.get('https://github-trending-api.now.sh/repositories', { searchParams }).json<Array<Repository>>()
-        localStorage.setItem('repos_params', spoken + language + since)
-        localStorage.setItem('repos_times', dayjs().toJSON())
-        localStorage.setItem('repos', JSON.stringify(data.slice(0,12)))
         actions.setRepositorys(data.slice(0,12))
+        // localStorage.setItem('repos_params', spoken + language + since)
+        // localStorage.setItem('repos_times', dayjs().toJSON())
+        // localStorage.setItem('repos', JSON.stringify(data.slice(0,12)))
       } catch (err) {
         console.error(err)
       }
       actions.setLoading(false)
-    }
+    // }
 
     const urlparams = new URLSearchParams()
     urlparams.set('since', since)
