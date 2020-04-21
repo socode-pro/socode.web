70245c4055cb501c98295999a4695ebdd957911f
cheney <zicjin@gmail.com>
2020-04-21 17:48:38 +0800

    temp

--- src/components/trending.tsx
+++ src/components/trending.tsx
@@ -19,7 +19,7 @@ const Trending: React.FC = (): JSX.Element => {
 
   useEffect(() => {
     fetch()
-  }, [fetch, programLanguage, since, spoken])
+  }, [])
 
   return (
     <div className={cs(css.trending)}>
