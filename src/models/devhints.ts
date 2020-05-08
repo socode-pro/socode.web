037fe3955e52e6930c59a15589b58fc863408f81
zicjin <zicjin@gmail.com>
2020-05-08 10:44:05 +0800

    temp

--- src/models/devhints.ts
+++ src/models/devhints.ts
@@ -6,8 +6,7 @@ export interface DevhintsModel {
   loading: boolean
   setLoading: Action<DevhintsModel, boolean>
   html: string
-  setHtml: Action<DevhintsModel, string>
-  setHtmlStorage: Action<DevhintsModel, string>
+  setHtml: Action<DevhintsModel, { html: string; storage?: boolean }>
   getHtml: Thunk<DevhintsModel>
 }
 
@@ -18,33 +17,27 @@ const devhintsModel: DevhintsModel = {
   }),
 
   html: '',
-  setHtml: action((state, html) => {
+  setHtml: action((state, { html, storage }) => {
     state.html = html
+    if (storage) {
+      localStorage.setItem('devhints_html', html)
+      localStorage.setItem('devhints_time', dayjs().toJSON())
+    }
   }),
-  setHtmlStorage: action((state, html) => {
-    localStorage.setItem('devhints_html', html)
-    state.html = html
-    localStorage.setItem('devhints_time', dayjs().toJSON())
-  }),
-  getHtml: thunk(async actions => {
+  getHtml: thunk(async (actions) => {
     try {
       const time = localStorage.getItem('devhints_time')
-      if (
-        time &&
-        dayjs(time)
-          .add(7, 'day')
-          .isAfter(dayjs())
-      ) {
+      if (time && dayjs(time).add(7, 'day').isAfter(dayjs())) {
         const devhintsHtml = localStorage.getItem('devhints_html')
         if (devhintsHtml) {
-          actions.setHtml(devhintsHtml || '')
+          actions.setHtml({ html: devhintsHtml })
           return
         }
       }
 
       actions.setLoading(true)
       const html = await ky.get('https://devhints.io/').text()
-      await actions.setHtmlStorage(html)
+      await actions.setHtml({ html, storage: true })
     } catch (err) {
       console.warn('DevhintsModel.getHtml:', err)
     }
