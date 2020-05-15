d65e32fe649066a52cd57888bc3630640e56d147
zicjin <zicjin@gmail.com>
2020-05-16 00:42:48 +0800

    password

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Search from './components/search'
-import Drawer from './components/drawer'
-import ExtArrow from './components/extarrow'
-import { isInStandaloneMode, isFirefox } from './utils/assist'
-import './styles/app.global.scss'
+import React from "react"
+import Search from "./components/search"
+import Drawer from "./components/drawer"
+import ExtArrow from "./components/extarrow"
+import { isInStandaloneMode } from "./utils/assist"
+import "./styles/app.global.scss"
 
 const Home: React.FC = () => {
   return (
     <>
       <Search />
       <Drawer />
-      {!isInStandaloneMode && !isFirefox && <ExtArrow />}
+      {!isInStandaloneMode && <ExtArrow />}
     </>
   )
 }
