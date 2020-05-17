7466cfed50ff2ae166fc5f3178525f45f10f5336
zicjin <zicjin@gmail.com>
2020-05-17 15:45:38 +0800

    temp

--- src/components/password.tsx
+++ src/components/password.tsx
@@ -1,40 +1,75 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useEffect } from "react"
+import ClipboardJS from "clipboard"
 import cs from "classnames"
 import css from "./password.module.scss"
 
-const passwordLengths = Array.from(Array(25).keys()).map((n) => n + 6)
-const letters = "abcdefghijklmnopqrstuvwxyz"
-const capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const number = "0123456789"
+const passwordLengths = Array.from(Array(27).keys()).map((n) => n + 6)
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789"
 const special = "!@#$%^&*()_+~`|}{[]:;?><,./-="
+
+const random = (): number => {
+  const { crypto, Uint32Array } = window
+  if (typeof crypto?.getRandomValues === "function" && typeof Uint32Array === "function") {
+    // Divide a random UInt32 by the maximum value (2^32 -1) to get a result between 0 and 1
+    return crypto.getRandomValues(new Uint32Array(1))[0] / 4294967295
+  }
+
+  return Math.random()
+}
+
+const keyGen = (length, useLowerCase = true, useUpperCase = true, useNumbers = true, useSpecial = true): string => {
+  let chars = ""
+  let key = ""
+
+  if (useLowerCase) chars += lowerCase
+  if (useUpperCase) chars += upperCase
+  if (useNumbers) chars += numbers
+  if (useSpecial) chars += special
+
+  Array.from(Array(length).keys()).forEach(() => {
+    key += chars[Math.floor(random() * chars.length)]
+  })
+
+  return key
+}
 
 const Password: React.FC = (): JSX.Element => {
   const [length, setLength] = useState(8)
-  const [useCapital, setUseCapital] = useState(true)
+  const [useLowerCase, setUseLowerCase] = useState(true)
+  const [useUpperCase, setUseUpperCase] = useState(true)
   const [useDigits, setUseDigits] = useState(true)
   const [useSpecial, setUseSpecial] = useState(true)
   const [password, setPassword] = useState("")
+  const [tooltips, setTooltips] = useState(false)
 
-  const Generate = useCallback(() => {
-    let n = ""
-    for (n = ""; n.length < length; ) {
-      const rand = Math.floor(4 * Math.random()) + 1
-      if (rand === 1) {
-        const entity1 = Math.ceil(letters.length * Math.random() * Math.random())
-        n += letters.charAt(entity1)
-      } else if (rand === 2) {
-        const entity2 = Math.ceil(number.length * Math.random() * Math.random())
-        useDigits && (n += number.charAt(entity2))
-      } else if (rand === 3) {
-        const entity3 = Math.ceil(special.length * Math.random() * Math.random())
-        useSpecial && (n += special.charAt(entity3))
-      } else {
-        const entity4 = Math.ceil(capitalLetters.length * Math.random() * Math.random())
-        useCapital && (n += capitalLetters.charAt(entity4))
-      }
-    }
-    setPassword(n)
-  }, [length, useCapital, useDigits, useSpecial])
+  useEffect(() => {
+    const clipboard = new ClipboardJS("#password")
+    clipboard.on("success", (e) => {
+      console.info("Text:", e.text)
+      if (e.text) setTooltips(true)
+    })
+  }, [])
+
+  const Generate = useCallback(
+    ({
+      _length = length,
+      _useLowerCase = useLowerCase,
+      _useUpperCase = useUpperCase,
+      _useDigits = useDigits,
+      _useSpecial = useSpecial,
+    }) => {
+      setLength(_length)
+      setUseLowerCase(_useLowerCase)
+      setUseUpperCase(_useUpperCase)
+      setUseDigits(_useDigits)
+      setUseSpecial(_useSpecial)
+      setPassword(keyGen(_length, _useLowerCase, _useUpperCase, _useDigits, _useSpecial))
+      setTooltips(false)
+    },
+    [length, useDigits, useLowerCase, useSpecial, useUpperCase]
+  )
 
   return (
     <div className={cs(css.password)}>
@@ -55,12 +90,23 @@ const Password: React.FC = (): JSX.Element => {
         <div className="">
           <input
             className="is-checkradio is-circle"
-            id="capital"
+            id="lowerCase"
             type="checkbox"
-            checked={useCapital}
-            onChange={(e) => setUseCapital(e.target.checked)}
+            checked={useLowerCase}
+            onChange={(e) => setUseLowerCase(e.target.checked)}
           />
-          <label htmlFor="capital">Capital Letters(A..Z)</label>
+          <label htmlFor="lowerCase">LowerCase Letters(a..z)</label>
+        </div>
+
+        <div className="mgl5">
+          <input
+            className="is-checkradio is-circle"
+            id="upperCase"
+            type="checkbox"
+            checked={useUpperCase}
+            onChange={(e) => setUseUpperCase(e.target.checked)}
+          />
+          <label htmlFor="upperCase">UpperCase Letters(A..Z)</label>
         </div>
 
         <div className="mgl5">
@@ -86,16 +132,58 @@ const Password: React.FC = (): JSX.Element => {
         </div>
       </div>
 
-      <a className="button is-primary mgt20" onClick={Generate}>
-        Generate
-      </a>
+      <div className="mgt20">
+        <a className="button is-primary mgb10" onClick={Generate}>
+          Generate
+        </a>
 
-      <input
-        className="input mgt20"
-        onClick={(e) => (e.target as HTMLInputElement).select()}
-        value={password}
-        onChange={() => null}
-      />
+        <a
+          className="button mgl10 mgb10"
+          onClick={() =>
+            Generate({ _length: 10, _useLowerCase: true, _useUpperCase: true, _useDigits: true, _useSpecial: false })
+          }>
+          Memorable Passwords
+        </a>
+        <a
+          className="button mgl10 mgb10"
+          onClick={() =>
+            Generate({ _length: 15, _useLowerCase: true, _useUpperCase: true, _useDigits: true, _useSpecial: true })
+          }>
+          Strong Passwords
+        </a>
+        <a
+          className="button mgl10 mgb10"
+          onClick={() =>
+            Generate({ _length: 30, _useLowerCase: true, _useUpperCase: true, _useDigits: true, _useSpecial: true })
+          }>
+          Fort Knox Passwords
+        </a>
+        <a
+          className="button mgl10 mgb10"
+          onClick={() =>
+            Generate({ _length: 32, _useLowerCase: true, _useUpperCase: true, _useDigits: true, _useSpecial: false })
+          }>
+          CodeIgniter Encryption Keys
+        </a>
+        <a
+          className="button mgl10 mgb10"
+          onClick={() =>
+            Generate({ _length: 20, _useLowerCase: true, _useUpperCase: true, _useDigits: true, _useSpecial: true })
+          }>
+          160-bit WPA Key
+        </a>
+      </div>
+
+      <div className={cs(css.passwordInput, "has-tooltip-warning")} data-tooltip={tooltips ? "Copied!" : null}>
+        <input
+          id="password"
+          className="input"
+          onClick={(e) => (e.target as HTMLInputElement).select()}
+          value={password}
+          onChange={() => null}
+          data-clipboard-target="#password"
+        />
+      </div>
     </div>
   )
 }
