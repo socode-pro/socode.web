99c37e3342a63a1e7e16b3c612d9ecb7002ce52d
zicjin <zicjin@gmail.com>
2020-06-11 16:24:40 +0800

    temp

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
-# To be verified: https://stackoverflow.com/a/22340057
+# To be verified: https://stackoverflow.com/a/47800439/346701
 cd dist
-find . -path ./.git -prune -o \( \! -path ./privacy \) -exec rm -rf {} \; 2> /dev/null
+find . -path ./.git -prune -o -path ./privacy -prune -o -path ./extension -prune -o -exec rm -rf {} \; 2> /dev/null
 cd ..
 
 cp -TR ./build/ ./dist/
 cp -TR ./build/index.html ./dist/privacy/index.html
+cp -TR ./build/index.html ./dist/extension/index.html
 cd  ./dist
 git add .
 git commit -a -m 'publish'
