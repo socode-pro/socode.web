3aa140d31ec10c4b48b275ad4afcc64507dd5e6b
zicjin <zicjin@gmail.com>
2020-06-11 16:24:40 +0800

    temp

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,7 @@
-Get-ChildItem -Path ".\dist\" -Exclude .git,privacy | Remove-Item -Recurse -Force
+Get-ChildItem -Path ".\dist\" -Exclude .git, privacy, extension | Remove-Item -Recurse -Force
 Copy-Item -Path ".\build\*" -Recurse -Destination ".\dist\" -Force
 Copy-Item -Path ".\build\index.html" -Recurse -Destination ".\dist\privacy\index.html" -Force
+Copy-Item -Path ".\build\index.html" -Recurse -Destination ".\dist\extension\index.html" -Force
 Set-Location .\dist
 git add .
 git commit -a -m 'publish'
