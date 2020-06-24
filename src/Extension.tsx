c79002999c4edf2339de72318a091541ff97b204
zicjin <zicjin@gmail.com>
2020-06-24 10:05:59 +0800

    temp

--- src/Extension.tsx
+++ src/Extension.tsx
@@ -79,89 +79,107 @@ const Extension: React.FC = () => {
   )
 
   return (
-    <div className={cs("container", css.extension)}>
-      <Brand />
-      <div className="columns">
-        <div className="column is-two-fifths">
-          <h1 className={css.title}>Search Documents in Address Bar</h1>
-          <p className={css.description}>{useIntl(Words.Description)}</p>
-          <animated.div
-            className={cs(css.wapper, css.browser)}
-            style={{
-              transform: to(
-                [xys],
-                (_xys): string => `perspective(60px) rotateX(${_xys[0]}deg) rotateY(${_xys[1]}deg) scale(${_xys[2]})`
-              ),
-            }}
-            // 362 是目标的横轴线离屏幕上边距距离。纵轴线在这里无法计算 https://codesandbox.io/embed/rj998k4vmm
-            onMouseMove={({ clientX: x, clientY: y }) => setXys({ xys: [-(y - 362) / 20, 0, 1.05] })}
-            onMouseLeave={() => setXys({ xys: [0, 0, 1] })}>
-            <a
-              className={cs(css.arrow, {
-                [css.edge]: isEdgeChromium && !ousideFirewall,
-                [css.firefox]: isFirefox,
-              })}
-              href={
-                isFirefox
-                  ? "https://addons.mozilla.org/zh-CN/firefox/addon/socode/"
-                  : // : isEdgeChromium && !ousideFirewall
-                  // ? "https://microsoftedge.microsoft.com/addons/detail/dkeiglafihicmjbbaoopggfnifgjekcl"
-                  ousideFirewall
-                  ? "https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/"
-                  : "https://www.crx4chrome.com/crx/196956/"
-              }>
-              <h3>Install to {isFirefox ? "Firefox" : isEdgeChromium && !ousideFirewall ? "Edge" : "Chrome"}</h3>
-            </a>
-          </animated.div>
-          <p className={css.otherlinks}>
-            {isFirefox ? <FontAwesomeIcon icon={faChrome} /> : <FontAwesomeIcon icon={faFirefox} />}
-            <a
-              href={
-                isFirefox
-                  ? "https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/"
-                  : "https://addons.mozilla.org/zh-CN/firefox/addon/socode/"
-              }>
-              Install to {isFirefox ? "Chrome" : "Firefox"}
-            </a>
-          </p>
-        </div>
-        <div className="column">
-          <div className={css.preview}>
-            <img src={`${process.env.REACT_APP_KEYS_HOST}/address.gif`} alt="preview" />
+    <>
+      <div className={cs("container", css.extension)}>
+        <Brand />
+        <div className="columns">
+          <div className="column is-two-fifths">
+            <h1 className={css.title}>Search Documents in Address Bar</h1>
+            <p className={css.description}>{useIntl(Words.Description)}</p>
+            <animated.div
+              className={cs(css.wapper, css.browser)}
+              style={{
+                transform: to(
+                  [xys],
+                  (_xys): string => `perspective(60px) rotateX(${_xys[0]}deg) rotateY(${_xys[1]}deg) scale(${_xys[2]})`
+                ),
+              }}
+              // 362 是目标的横轴线离屏幕上边距距离。纵轴线在这里无法计算 https://codesandbox.io/embed/rj998k4vmm
+              onMouseMove={({ clientX: x, clientY: y }) => setXys({ xys: [-(y - 362) / 20, 0, 1.05] })}
+              onMouseLeave={() => setXys({ xys: [0, 0, 1] })}>
+              <a
+                className={cs(css.arrow, {
+                  [css.edge]: isEdgeChromium && !ousideFirewall,
+                  [css.firefox]: isFirefox,
+                })}
+                href={
+                  isFirefox
+                    ? "https://addons.mozilla.org/zh-CN/firefox/addon/socode/"
+                    : // : isEdgeChromium && !ousideFirewall
+                    // ? "https://microsoftedge.microsoft.com/addons/detail/dkeiglafihicmjbbaoopggfnifgjekcl"
+                    ousideFirewall
+                    ? "https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/"
+                    : "https://www.crx4chrome.com/crx/196956/"
+                }>
+                <h3>Install to {isFirefox ? "Firefox" : isEdgeChromium && !ousideFirewall ? "Edge" : "Chrome"}</h3>
+              </a>
+            </animated.div>
+            <p className={css.otherlinks}>
+              {isFirefox ? <FontAwesomeIcon icon={faChrome} /> : <FontAwesomeIcon icon={faFirefox} />}
+              <a
+                href={
+                  isFirefox
+                    ? "https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/"
+                    : "https://addons.mozilla.org/zh-CN/firefox/addon/socode/"
+                }>
+                Install to {isFirefox ? "Chrome" : "Firefox"}
+              </a>
+            </p>
+          </div>
+          <div className="column">
+            <div className={css.preview}>
+              <img src={`${process.env.REACT_APP_KEYS_HOST}/address.gif`} alt="preview" />
+            </div>
           </div>
         </div>
       </div>
 
-      {/* <h2 className={css.devdocsHeader}>Supported</h2> */}
-      <div className={css.devdocsKeys}>{keysDom(devdocsKeys)}</div>
+      <div className={css.keysWapper}>
+        {/* <h2 className={css.devdocsHeader}>Supported</h2> */}
+        <div className={css.devdocsKeys}>{keysDom(devdocsKeys)}</div>
+      </div>
 
-      <footer className={cs(css.footer)}>
-        <a
-          className={cs(css.navlink, css.telegram)}
-          href="https://t.me/SocodePro"
-          target="_blank"
-          rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faTelegram} />
-        </a>
-        <a
-          className={cs(css.navlink, css.discord)}
-          href="https://discord.gg/QeuD8Ma"
-          target="_blank"
-          rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faDiscord} />
-        </a>
-        <a
-          className={cs(css.navlink, css.twitter)}
-          href="https://twitter.com/socode7"
-          target="_blank"
-          rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-        <a className={cs(css.navlink, css.email)} href="mailto:elliotreborn@gmail.com">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
-      </footer>
-    </div>
+      <div className={cs("container")}>
+        <footer className={cs(css.footer)}>
+          <div className={css.infos}>
+            {language === InterfaceLanguage.中文 && (
+              <p className="mgt10">
+                <a href="http://beian.miit.gov.cn/state/outPortal/loginPortal.action">苏ICP备18044337号-2</a>
+              </p>
+            )}
+            <p className={css.slogon}>Hack your life</p>
+            <p className={css.principles}>and become a professional mistake maker.</p>
+          </div>
+          <div className={css.links}>
+            <a
+              className={cs(css.navlink, css.telegram)}
+              href="https://t.me/SocodePro"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTelegram} />
+            </a>
+            <a
+              className={cs(css.navlink, css.discord)}
+              href="https://discord.gg/QeuD8Ma"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faDiscord} />
+            </a>
+            <a
+              className={cs(css.navlink, css.twitter)}
+              href="https://twitter.com/socode7"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a className={cs(css.navlink, css.email)} href="mailto:elliotreborn@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </div>
+        </footer>
+      </div>
+
+    </>
   )
 }
 
