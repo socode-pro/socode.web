3473ce9c58ca811b71b97e95f7e885579b36ba60
jojo <xiongjiajia1126@gmail.com>
2020-07-01 13:16:50 +0800

    copyitem

---
+++ copyitem.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path ".\build\index.html" -Recurse -Destination ".\build\privacy\index.html" -Force
+Copy-Item -Path ".\build\index.html" -Recurse -Destination ".\build\extension\index.html" -Force
---
+++ copyitem.sh
@@ -0,0 +1,4 @@
+mkdir -p ./build/privacy
+mkdir -p ./build/extension
+cp ./build/index.html ./build/privacy/index.html
+cp ./build/index.html ./build/extension/index.html
