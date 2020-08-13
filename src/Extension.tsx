075041de5499651097df62aa76647ac7295407c4
zicjin <zicjin@gmail.com>
2020-08-13 09:52:01 +0800

    temp

--- src/Extension.tsx
+++ src/Extension.tsx
@@ -113,8 +113,7 @@ const Extension: React.FC = () => {
                     // ? "https://microsoftedge.microsoft.com/addons/detail/dkeiglafihicmjbbaoopggfnifgjekcl"
                     ousideFirewall
                     ? "https://chrome.google.com/webstore/detail/hlkgijncpebndijijbcakkcefmpniacd/"
-                      :
-                      "https://www.crx4chrome.com/crx/196956/"
+                    : "https://www.crx4chrome.com/crx/196956/"
                 }>
                 <h3>Install to {isFirefox ? "Firefox" : isEdgeChromium && !ousideFirewall ? "Edge" : "Chrome"}</h3>
               </a>
