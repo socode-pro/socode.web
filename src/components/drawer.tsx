25d236704c9f80a62c4e6e778a0656b2353b044b
Cheney Jin <zicjin@gmail.com>
2021-08-11 11:39:47 +0800

    remove china record

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -410,12 +410,7 @@ const Drawer: React.FC = (): JSX.Element => {
             />
           </div> */}
 
-          {/* <a>投放广告</a> */}
-          {language === InterfaceLanguage.中文 && (
-            <a className="mgl10" href="http://beian.miit.gov.cn/state/outPortal/loginPortal.action">
-              苏ICP备18044337号-2
-            </a>
-          )}
+          {/* <a>Sponsor</a> */}
           <p className={css.slogon}>Hack your life</p>
           <p className={css.principles}>and become a professional mistake maker.</p>
         </footer>
